6ebafe8f9199f0cdc28d5605da56f51759a72278
Greg Bahm <gregbahm@gmail.com>
2017-07-12 10:59:34 -0700

    Adding tester script to figure out that weird bug.

---
+++ Assets/TesterScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterScript : MonoBehaviour 
+{
+    public PaintbrushScript Brush;
+    public Transform PaintbrushTip;
+    public Transform ColorPickerTip;
+    
+	// Update is called once per frame
+	void Update () 
+    {
+        float weight = Mathf.Sin(UnityEngine.Time.fixedTime) / 2 + .5f;
+        Brush.StrokeWeight = weight;
+	}
+}
